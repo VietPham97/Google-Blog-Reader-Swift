375bb0f8a56d43b5546347c715c967ffebe643fb
Viet Pham <vietpham1097@gmail.com>
2018-03-31 23:48:15 -0400

    Application completed

--- Blog Reader/DetailViewController.swift
+++ Blog Reader/DetailViewController.swift
@@ -7,38 +7,51 @@
 //
 
 import UIKit
+import WebKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, WKNavigationDelegate
+{
+   let contentKey = "content"
+   let titleKey = "title"
+   
+   var webView: WKWebView!
+   
+   func configureView()
+   {
+      // Update the user interface for the detail item.
+      if let detail = self.detailItem
+      {
+         self.title = detail.value(forKey: self.titleKey) as! String
+         if let blogWebView = self.webView
+         {
+            blogWebView.loadHTMLString(detail.value(forKey: contentKey) as! String, baseURL: nil)
+         }
+      }
+   }
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
+   override func loadView() {
+      webView = WKWebView()
+      webView.navigationDelegate = self
+      view = webView
+   }
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+      configureView()
+   }
 
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
 
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.timestamp!.description
-            }
-        }
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        configureView()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    var detailItem: Event? {
-        didSet {
-            // Update the view.
-            configureView()
-        }
-    }
+   var detailItem: Event? {
+      didSet {
+         // Update the view.
+         configureView()
+      }
+   }
 
 
 }
